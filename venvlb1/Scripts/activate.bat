2dd9f1c0891a5751b498a568b1722d5f5f8fe809
Nana_OA <you@example.com>
2018-09-15 00:49:55 -0400

    Initial commit

---
+++ venvlb1/Scripts/activate.bat
@@ -0,0 +1,30 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\Nana_OA\DOCUME~1\WEBCLA~1\CSCI E-14a Web Apps for Data Analysis\Lab_Files\lab1\venvlb1"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+)
+set "PROMPT=(venvlb1) %PROMPT%"
+
+REM Don't use () to avoid problems with them in %PATH%
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
+
+set PYTHONHOME=
+
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
